c95cf2fe0af2d568b26bfa6c239f5a4d07016c3f
Yuvraj <110035028+Yuvrajsoni05@users.noreply.github.com>
2025-11-16 00:37:57 +0530

    live

--- Resume/settings.py
+++ Resume/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-9g3bmdz6ew-dn99hh5nrx!o8t^v3=uvgd9r&d4=%bd=9#tcht-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
